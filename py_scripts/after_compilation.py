556f8204b9ae61cd04751c8b6f08e4362c007afc
Alexandrov Oleg <olegzandroff@gmail.com>
2020-12-23 08:29:56 +0300

    удалены ненужные скрипты

--- py_scripts/after_compilation.py
+++
@@ -1,5 +0,0 @@
-path = "../lab_1"
-
-with open(f"{path}/prototypes_backup", "r") as backup:
-    with open(f"{path}/prototypes.h", "w") as prototypes:
-        prototypes.writelines(backup.readlines())
